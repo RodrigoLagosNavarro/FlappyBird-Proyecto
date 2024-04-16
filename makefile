b2880aacd85f9ac633bbb7aec6b055d672fcc3d8
RodrigoLagosNavarro <159966435+RodrigoLagosNavarro@users.noreply.github.com>
2024-04-16 23:43:58 +0000

    Version 2

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 bin/test:src/tui_test.cpp
-	g++ src/tui_test.cpp -o bin/test -std=c++2a -lf
+	g++ src/tui_test.cpp -o bin/test -std=c++2a -lftxui-screen -lftxui-component -lftxui-dom
 
-ejecutar: bin/test
+run : bin/test
 	./bin/test
 
--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -1,20 +1,24 @@
-#include <ftxui/dom/node.hpp>
-#include<ftxui/component/screen.hpp>
-#include<ftxui/screen/string.hpp>
-
-using namespace fxtui;
-
-int main(int argc, char const *argv[])
-{
-    Element label=text("Hola mundo");
-    Element container=hbox({label});
-
-    auto screen=Screen::Create(
-        Dimension::Full(),
-        Dimension::Fit(container)
-    );
-
-    Render(screen,container);
-    screen.Print();
-    return 0;
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <iostream>
+ 
+int main(void) {
+  using namespace ftxui;
+ 
+  // Define the document
+  Element document =
+    hbox({
+      text("left")   | border,
+      text("middle") | border | flex,
+      text("right")  | border,
+    });
+ 
+  auto screen = Screen::Create(
+    Dimension::Full(),       // Width
+    Dimension::Fit(document) // Height
+  );
+  Render(screen, document);
+  screen.Print();
+ 
+  return EXIT_SUCCESS;
 }
