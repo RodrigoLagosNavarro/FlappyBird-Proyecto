0a21b21d858986e318d44888ccf3cf56726163ba
RodrigoLagosNavarro <159966435+RodrigoLagosNavarro@users.noreply.github.com>
2024-04-23 00:20:47 +0000

    Version 5

--- makefile
+++ makefile
@@ -1,11 +1,11 @@
 bin/test:src/tui_test.cpp
-	g++ src/tui_test.cpp -o bin/test -std=c++2a -lftxui-screen -lftxui-component -lftxui-dom
+	g++ src/tui_test.cpp -o bin/test -std=c++2a -lftxui-screen -lftxui-component -lftxui-dom -Iinclude
 
 run : bin/test
 	./bin/test
 
 bin/space:src/main.cpp
-	g++ src/main.cpp -o bin/space -std=c++2a -lftxui-screen -lftxui-component -lftxui-dom
+	g++ src/main.cpp -o bin/space -std=c++2a -lftxui-screen -lftxui-component -lftxui-dom -Iinclude
 
 runTest: bin/space
 	./bin/space
--- src/main.cpp
+++ src/main.cpp
@@ -3,42 +3,43 @@
 #include<thread>
 #include<ftxui/dom/elements.hpp>
 #include<ftxui/screen/screen.hpp>
+#include<ftxui/screen/string.hpp>
+#include<ftxui/screen/terminal.hpp>
+#include<fstream>
+#include<Dibujo.hpp>
 
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    fstream canon;
-    canon.open("./assets/images/canon.txt");
-    string canon;
-    archivo>>canon;
-    archivo.close();
 
-    archivo.open("./assets/images/alien.txt");
+    Dibujo dTanque("./assets/images/canon.txt");
+    Dibujo dAlien("./assets/images/alien.txt");
+    Dibujo dBloque("./assets/images/canon.txt");
 
-    fstream alien;
-    alien.open("./assets/images/alien.txt");
-    archivo>>alien;
-    archivo.close();
+
 
     int fotograma=0;
+    while(true){
 
-    while(true){   
         fotograma++;
-    Element personaje=hbox({spinner(21,fotograma) | bold | color(Color::Blue1) | bgcolor(Color::Yellow1) });
-    Element tanque=text(canon) | bold | color(Color::Green) | bgcolor(Color::Blue) });
-    Element lienzo=hbox({personaje,tanque});
+        Element personaje = spinner(21,fotograma) | bold | color(Color::Blue1) | bgcolor(Color::Green1);
+        Element tanque = dTanque.GetElement() | bold | color(Color::Green1) | bgcolor(Color::Blue1) ;
+        Element lienzo = hbox({personaje , tanque, dAlien.GetElement()});
 
-    Screen pantalla= Screen::Create(
-        Dimension::Full(),
-        Dimension::Fit(lienzo)
-        );
-    Render(pantalla,lienzo);
-    pantalla.Print();
-    cout<<pantalla.ResetPosition();
-    this_thread::sleep_for(0.1s);
-    }
+        Screen pantalla = Screen::Create(
+            Dimension::Full(),
+            Dimension::Fit(lienzo)
+             );
 
+
+
+        Render(pantalla,lienzo);
+        pantalla.Print();
+        cout<<pantalla.ResetPosition();
+
+        this_thread::sleep_for(0.1s);
+     }
     return 0;
 }
